435220cab3face02fac20560e2655a898dfa0ac1
Yusuf Rizal <yusufrizalh@gmail.com>
2021-11-18 10:34:00 +0700

    async await function

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,8 @@
 /*
     # Function di JavaScript
       > callback    : procedure & function
-      > promises
-      > async await
+      > promises    : menjanjikan proses
+      > async await : kemampuan untuk menunggu proses sebelumnya
 */
 
 function penjumlahan(nilai1, nilai2) {
@@ -16,4 +16,46 @@ function tampilkan(nilai1, nilai2, hitung) {
 }
 
 var tampilPenjumlahan = tampilkan(10, 25, penjumlahan);
-console.log("Hasil Penjumlahan: " + tampilPenjumlahan);
+// console.log("Hasil Penjumlahan: " + tampilPenjumlahan);
+
+// ------------------------------------------------------------
+
+let promise = new Promise(function (resolve, reject) {
+  let flag = true;
+  setTimeout(() => {
+    if (flag) {
+      resolve("Berhasil");
+    } else {
+      reject("Gagal");
+    }
+  }, 2000);
+});
+
+promise
+  .then(function (value) {
+    console.log(value);
+  })
+  .catch(function (value) {
+    console.log(value);
+  });
+
+// ------------------------------------------------------------
+
+async function ambilGambar() {
+  let response = await fetch("../images/pexels-image-1.jpg");
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  let myImage = await response.blob();
+
+  let imageUrl = URL.createObjectURL(myImage);
+  let image = document.createElement("img");
+  image.src = imageUrl;
+  document.body.appendChild(image);
+}
+
+ambilGambar().catch((error) => {
+  console.log("There is a problem with your fetch! " + error.message);
+});
